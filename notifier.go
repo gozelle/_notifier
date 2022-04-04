7565c264581d9ca69c58e44d4900a7e491c6303d
koyeo <im@koyeo.io>
2022-04-04 22:36:11 +0800

    update api

--- notifier.go
+++ notifier.go
@@ -16,31 +16,40 @@ type Notifier struct {
 }
 
 // SetMuteDuration 设置沉默时间，在此时间段内，调用 ReTrigger() 将得到 false
-func (p *Notifier) SetMuteDuration(muteDuration time.Duration) {
+func (p *Notifier) SetMuteDuration(muteDuration time.Duration) *Notifier {
 	p.muteDuration = muteDuration
+	return p
 }
 
-// Trigger 触发通知
-func (p *Notifier) Trigger() {
+// FromNow 获取首次触发到现在的时间间隔
+func (p *Notifier) FromNow() time.Duration {
+	if p.triggeredAt == nil {
+		return 0
+	}
+	return time.Since(*p.triggeredAt)
+}
+
+// Sent 标记通知已发送状态
+func (p *Notifier) Sent() {
 	now := time.Now()
 	p.triggeredAt = &now
 }
 
-// Triggered 获取首次是否已经触发的状态
-func (p *Notifier) Triggered() bool {
+// Empty 判断通知器是否为空状态（未触发过的状态）
+func (p *Notifier) Empty() bool {
 	return p.triggeredAt != nil
 }
 
-// Reset 重置通知器
-func (p *Notifier) Reset() {
+// Clear 重置通知器状态
+func (p *Notifier) Clear() {
 	p.triggeredAt = nil
 	p.reTriggerAt = nil
 }
 
-// ReTrigger 获取是否再次触发
+// Remind 获取是否到达发送提醒的时间点
 // 获取再次触发后，静默周期将从头计算。
 // 后续的触发动作有必要保证触发成功，否则将在下个周期获得重新触发的机会
-func (p *Notifier) ReTrigger() bool {
+func (p *Notifier) Remind() bool {
 	if p.triggeredAt == nil {
 		return false
 	}
