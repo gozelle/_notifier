5b2a2af77f661e642d23a2593ba2cb772302d1ad
koyeo <im@koyeo.io>
2022-04-04 22:42:51 +0800

    update api

--- notifier.go
+++ notifier.go
@@ -10,9 +10,19 @@ func NewNotifier() *Notifier {
 
 // Notifier 监控通知器
 type Notifier struct {
-	triggeredAt    *time.Time
-	reTriggerAt    *time.Time
+	sentAt         *time.Time
+	remindAt       *time.Time
 	remindDuration time.Duration
+}
+
+// RemindAt 获取上一次提醒的时间
+func (p *Notifier) RemindAt() *time.Time {
+	return p.remindAt
+}
+
+// SentAt 获取首次触发的时间
+func (p *Notifier) SentAt() *time.Time {
+	return p.sentAt
 }
 
 // SetRemindDuration 设置提醒周期，在此时间段内，调用 Remind() 将得到 false
@@ -23,45 +33,45 @@ func (p *Notifier) SetRemindDuration(d time.Duration) *Notifier {
 
 // FromNow 获取首次触发到现在的时间间隔
 func (p *Notifier) FromNow() time.Duration {
-	if p.triggeredAt == nil {
+	if p.sentAt == nil {
 		return 0
 	}
-	return time.Since(*p.triggeredAt)
+	return time.Since(*p.sentAt)
 }
 
 // Sent 标记通知已发送状态
 func (p *Notifier) Sent() {
 	now := time.Now()
-	p.triggeredAt = &now
+	p.sentAt = &now
 }
 
 // Empty 判断通知器是否为空状态（未触发过的状态）
 func (p *Notifier) Empty() bool {
-	return p.triggeredAt != nil
+	return p.sentAt != nil
 }
 
 // Clear 重置通知器状态
 func (p *Notifier) Clear() {
-	p.triggeredAt = nil
-	p.reTriggerAt = nil
+	p.sentAt = nil
+	p.remindAt = nil
 }
 
 // Remind 获取是否到达发送提醒的时间点
 // 获取再次触发后，静默周期将从头计算。
 // 后续的触发动作有必要保证触发成功，否则将在下个周期获得重新触发的机会
 func (p *Notifier) Remind() bool {
-	if p.triggeredAt == nil {
+	if p.sentAt == nil {
 		return false
 	}
-	if p.reTriggerAt == nil {
-		p.reTriggerAt = p.triggeredAt
+	if p.remindAt == nil {
+		p.remindAt = p.sentAt
 	}
-	if p.remindDuration == 0 || p.reTriggerAt == nil {
+	if p.remindDuration == 0 || p.remindAt == nil {
 		return false
 	}
-	if uint64(time.Since(*p.reTriggerAt)) > uint64(p.remindDuration) {
+	if uint64(time.Since(*p.remindAt)) > uint64(p.remindDuration) {
 		now := time.Now()
-		p.reTriggerAt = &now
+		p.remindAt = &now
 		return true
 	}
 	return false
